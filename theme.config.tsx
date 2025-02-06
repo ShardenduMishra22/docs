ca1b7fa88fbeab8a4aaf3daa78df4db49b2b46c0
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2025-02-06 14:14:43 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,15 +8,17 @@ const config: DocsThemeConfig = {
       <img
         src="http://sanity-admin.rudraksha-ratna.com/static/images/blogs/lord-parashuram(1).jpg"
         alt="Team Parshu"
-        style={{ height: '30px', width: '30px', borderRadius: '50%' }}
+        style={{ height: '30px', width: '30px', borderRadius: '50%' }}  // Round image
       />
       <span style={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#f56565' }}>BloodSphere</span>
     </div>
+  ),
+  project: {
     link: 'https://github.com/MishraShardendu22',
   },
   chat: {
     link: 'https://www.linkedin.com/in/shardendumishra22/',
-    icon: <Linkedin size={20} />, 
+    icon: <Linkedin size={20} />,  // LinkedIn icon from lucide-react
   },
   footer: {
     text: 'IIIT Dharwad - Team ParshuVelocity',
